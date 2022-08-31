a6809092097385f5eebdcf997b6647c364b0015f
webferrol <xurxo@webferrol.com>
2022-08-31 23:44:01 +0200

    Obtención de los datos con la api de exchangeratesapi

--- README.md
+++ README.md
@@ -5,11 +5,16 @@ En el enlace anterior viene como es la instalación. Tener en cuenta que esto ca
 
 ## Proyecto
 Seguiremos el proyecto de **Udemy** titulado [Sección 34: [Vue.js 2] AXIOS y VUETIFY](https://www.udemy.com/course/curso-vue/learn/lecture/13546112#announcements/6865968/)
-Se realizará un _calendario_ donde se gestionará el valor del dolar
+Se realizará un _calendario_ donde se gestionará el valor del dolar. La obtención de los datos se realizará en [exchangeratesapi](https://exchangeratesapi.io/?utm_source=envatotuts&utm_medium=ThirdParties&utm_content=Tutorial) por problemas de sobrecarga de la página de la alternativa ofreciad por udemy [mindicador](https://mindicador.cl)
 
 ## Vue 3 | Calendar
 Importante ir a la [página](https://vcalendar.io/vue-3.html) para ver como es su instalación y configuración. 
 **Hubo que realizar esta instalación puesto que v-date-picker de _Vuetify_ en su versión beta no funciona para esta api**
+Usamos el componente **Datepicker**. Ver [API](https://vcalendar.io/api/v2.0/datepicker.html)
 
 ## Vue AXIOS
 Importante leer la documentación de instalación y configuración de axios (sobre todo en Composition API). Ver el siguiente [enlace](https://www.npmjs.com/package/vue-axios)
+
+## Obtención de datos
+[exchangeratesapi](https://exchangeratesapi.io/?utm_source=envatotuts&utm_medium=ThirdParties&utm_content=Tutorial)
+_Clave_: cWdCK2JRRNq1epc2G0d1dgfO7I6Xi0Sa
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,38 +1,85 @@
 <script setup>
-  import { ref } from "vue";
-  
-
-  import { DatePicker } from 'v-calendar';
-
-import MyCard from '../components/MyCard.vue';
+import { computed, ref } from "vue";
+import { DatePicker } from 'v-calendar';
+import axios from "axios";
 const date = ref(new Date());
+const valor = ref(0);
+const error = ref(false);
+const isClicked = ref(false); //Preguntamos si fue ya pulsada una fecha
+
+
+const dateString = computed(() => {
+  const [year,month,day] = date.value.toISOString().substring(0, 10).split("-");//destructuring from Array
+  return `${day}-${month}-${year}`;
+});
+
+const euroValue = computed(()=>`${valor.value} €`);
+
+/** Para desactivar el calendario necesitamos que el parámetro min-date del componente sea la fecha actual
+Este caso ocurrirá mientras esperamos a que los datos de la API sean enviandos
+*/
+const minDate = computed(() => (isClicked.value) ? new Date() : new Date(1999, 0, 1));
+
+/**
+ * Obtenemos el objeto desestructurado de la API ver documentación
+ * @param {String} {id} Objeto day desestructurado {id,...} = day. Formato id: yyyy-mm-dd
+ * @description Consultar por tipo de indicador económico dado un año determinado
+ * @url https://mindicador.cl/
+ */
+const handleDayClick = async ({ id }) => {
+  date.value = new Date(id);
+  if (isClicked.value) //Si aún no terminó una operación síncrona
+    return;
+  try {
+    isClicked.value = true;
+    //Con axios
+    const { data } = await axios.get(`https://api.apilayer.com/exchangerates_data/${id}?symbols=EUR&base=USD`, {
+      headers: {
+        "apikey": "cWdCK2JRRNq1epc2G0d1dgfO7I6Xi0Sa"
+      }
+    });
+    valor.value = data.rates.EUR;
+  } catch (err) {
+    error.value = err.message;
+  } finally {
+    isClicked.value = false;
+  }
+
+}
+
+handleDayClick({id:new Date().toISOString().substring(0,10)});
+//Sin axios
+// const fetchFunction = async () => {
+//   var myHeaders = new Headers();
+//   myHeaders.append("apikey", "cWdCK2JRRNq1epc2G0d1dgfO7I6Xi0Sa");
+//   var requestOptions = {
+//     method: 'GET',
+//     redirect: 'follow',
+//     headers: myHeaders
+//   };
+//   const response = await fetch(`https://api.apilayer.com/exchangerates_data/${id}?symbols=EUR&base=USD`, requestOptions);
+//   const data = await response.json();
+// }
 </script>
 <template>
   <v-container fluid>
     <v-row>
       <v-col cols="12">
-        
-        <DatePicker is-expanded v-model="date" />
-        
-       
+        <!-- DatePicker -->
+        <DatePicker @dayclick="handleDayClick" :min-date="minDate" :max-date='new Date()' is-expanded v-model="date" />
+        <!-- End DatePicker -->
       </v-col>
       <v-col cols="12">
-        <v-card>
+        <v-card :loading="isClicked" :loader-height="200">
           <v-card-title>
-            {{date}}
+            {{ dateString }}
           </v-card-title>
+          <v-card-text>
+            {{ euroValue }}
+            <v-alert v-if="error" border border-color="white" :text="error" type="error" title="Error" elevated></v-alert>
+          </v-card-text>
         </v-card>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi laudantium iusto facere, atque ab placeat at quae dolores nesciunt, eveniet quo distinctio? Earum quas totam enim nihil inventore consequuntur animi.</v-col>
+      </v-col>
     </v-row>
   </v-container>
-  
-          
 </template>
-
-<script setup>
-
-</script>
-
-<style lang="scss" scoped>
-
-</style>
